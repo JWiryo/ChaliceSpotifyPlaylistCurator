b1fab474b8f6a77109e6da6be3553f17c3115922
James Wiryo <jamesgiggs95@gmail.com>
2018-11-06 02:49:09 +0800

    Added delete tracks from playlist functionality to allow only top 20 tracks in playlist

--- PlaylistEditor/addTopTracksToPlaylist.py
+++ PlaylistEditor/addTopTracksToPlaylist.py
@@ -14,12 +14,28 @@ def add_top_tracks_to_playlist():
     access_token = get_new_access_token(oauth)
     sp = spotipy.Spotify(auth=access_token)
 
+    # Get Current Playlist Tracks
+    current_playlist_tracks = sp.user_playlist_tracks(constants.USERNAME, constants.PLAYLIST_ID, None, 20, 0, None)
+
+    # Delete all tracks from current playlist
+    list_of_tracks_to_delete = []
+    for track_to_delete in range(len(current_playlist_tracks['items'])):
+        url_of_track_to_delete = current_playlist_tracks['items'][track_to_delete]['track']['uri']
+        list_of_tracks_to_delete.append(url_of_track_to_delete)
+
+    sp.user_playlist_remove_all_occurrences_of_tracks(constants.USERNAME, constants.PLAYLIST_ID,
+                                                      list_of_tracks_to_delete)
+
     # Get Current Top Tracks
     list_top_tracks = sp.current_user_top_tracks(20, 0, 'short_term')
-    track_urls_list = []
-    for track in range(len(list_top_tracks['items'])):
-        track_url = list_top_tracks['items'][track]['uri']
-        track_urls_list.append(track_url)
 
     # Add Top Tracks to Playlist
-    sp.user_playlist_add_tracks(constants.USERNAME, constants.PLAYLIST_ID, track_urls_list)
+    list_of_track_urls = []
+    for track in range(len(list_top_tracks['items'])):
+        track_url = list_top_tracks['items'][track]['uri']
+        list_of_track_urls.append(track_url)
+
+    sp.user_playlist_add_tracks(constants.USERNAME, constants.PLAYLIST_ID, list_of_track_urls)
+
+
+add_top_tracks_to_playlist()
