c048441516260ba00fe73e4a79af4b39c5a79f87
James Wiryo <jamesgiggs95@gmail.com>
2018-11-08 23:58:40 +0800

    Add cron expression to schedule lambda function trigger every Monday 12AM SGT

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from chalicelib.PlaylistEditor import addTopTracksToPlaylist
 app = Chalice(app_name='spotifyAutoPlaylist')
 app.debug = True
 
-@app.route('/')
+# Trigger Lambda function every Monday at 00:00 AM (GMT +8)
+@app.schedule('cron(0 16 ? * SUN *)')
 def generate_top20_tracks_to_playlist():
     addTopTracksToPlaylist.add_top_tracks_to_playlist()
     return {'message': 'OK'}
