c688f780854dfb1a75dd35fa6f71d3cb5c2be951
James Wiryo <jamesgiggs95@gmail.com>
2018-11-12 00:37:38 +0800

    Fixed CloudWatch event not being able to trigger lambda function as no event argument was given to app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app.debug = True
 
 # Trigger Lambda function every Monday at 00:00 AM (GMT +8)
 @app.schedule('cron(0 16 ? * SUN *)')
-def generate_top20_tracks_to_playlist():
+def generate_top20_tracks_to_playlist(event):
     addTopTracksToPlaylist.add_top_tracks_to_playlist()
     return {'message': 'OK'}
